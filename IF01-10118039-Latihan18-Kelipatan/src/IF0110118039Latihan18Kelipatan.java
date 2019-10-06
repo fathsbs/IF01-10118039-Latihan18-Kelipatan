82322d35815616651aabbc717ff10b03dd49bc5b
Fatahillah Satria Bima Seno <55565303+fathsbs@users.noreply.github.com>
2019-10-06 07:53:09 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan18-Kelipatan/src/IF0110118039Latihan18Kelipatan.java
@@ -0,0 +1,19 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM : Program yang menampilkan kelipatan dari bilangan 3.5  
+ */
+
+public class IF0110118039Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
